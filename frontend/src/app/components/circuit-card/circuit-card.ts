75f818de37a7a22de9df3838f9dac0f8b7bbe052
Jotanore <jotanore@hotmail.com>
2026-08-03 17:14:24 +0200

    Circuit card using a signal

--- frontend/src/app/components/circuit-card/circuit-card.ts
+++ frontend/src/app/components/circuit-card/circuit-card.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Circuit } from '../../models/circuit.model';
 
 @Component({
   selector: 'app-circuit-card',
@@ -6,4 +7,11 @@ import { Component } from '@angular/core';
   templateUrl: './circuit-card.html',
   styleUrl: './circuit-card.css',
 })
-export class CircuitCard {}
+export class CircuitCard {
+
+  /*este circuit es el que se pasa desde el componente padre, en este caso circuit-list-page en [circuit]="circuit"
+  Se corresponde a [circuit], mientras que "circuit" es la iteración actual de
+  @for (circuit of circuits; track circuit.id)
+   */
+  circuit = input.required<Circuit>();
+}
