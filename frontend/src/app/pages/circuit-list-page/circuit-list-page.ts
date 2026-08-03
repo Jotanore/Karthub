00cbe9da92b5263b69681cfe46e9d30187276ece
Jotanore <jotanore@hotmail.com>
2026-08-03 17:14:50 +0200

    A list using circuit card component

--- frontend/src/app/pages/circuit-list-page/circuit-list-page.ts
+++ frontend/src/app/pages/circuit-list-page/circuit-list-page.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 //servicio inyectado para poder usarlo en el componente
 import { CircuitService } from '../../services/circuit';
 import { Circuit } from '../../models/circuit.model';
+import { CircuitCard } from '../../components/circuit-card/circuit-card';
 
 @Component({
   selector: 'app-circuit-list-page',
-  imports: [],
+  imports: [CircuitCard],
   templateUrl: './circuit-list-page.html',
   styleUrl: './circuit-list-page.css',
 })
