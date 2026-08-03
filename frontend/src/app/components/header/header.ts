d169299f02d18c97e90f1791706ffb476cfcd258
Jotanore <jotanore@hotmail.com>
2026-08-03 14:31:46 +0200

    Header with Routerlink and Active

--- frontend/src/app/components/header/header.ts
+++ frontend/src/app/components/header/header.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+//RouterLink permite usar enlaces sin recargar en Angular
+//RouterLinkActive permite saber si la ruta actual es la misma que la del enlace y así poder aplicar estilos condicionales
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header { }
