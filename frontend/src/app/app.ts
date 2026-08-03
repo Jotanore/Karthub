8b2f27a3b0ee5f89f40dc02002d8580f9405784e
Jotanore <jotanore@hotmail.com>
2026-08-03 14:31:26 +0200

    Header Import

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+//importar el ts del header para poder usarlo y añadirlo a imports
+import { Header } from './components/header/header'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
