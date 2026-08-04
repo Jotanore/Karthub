f2446d0fec3469a7cad676ac8ca6eb4b468a36c2
Jotanore <jotanore@hotmail.com>
2026-08-04 14:36:36 +0200

    Signals, Tool update & Toolbar Import

--- frontend/src/app/pages/racing-line-editor-page/racing-line-editor-page.ts
+++ frontend/src/app/pages/racing-line-editor-page/racing-line-editor-page.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { EditorTool } from '../../models/editor-tool.model';
+import { RacingLinePoint } from '../../models/racing-line-point.model';
+import { EditorToolbar } from '../../components/editor-toolbar/editor-toolbar';
 
 @Component({
   selector: 'app-racing-line-editor-page',
-  imports: [],
+  imports: [EditorToolbar],
   templateUrl: './racing-line-editor-page.html',
   styleUrl: './racing-line-editor-page.css',
 })
-export class RacingLineEditorPageComponent { }
+export class RacingLineEditorPageComponent {
+
+  //Esto es una señal readonly que evita reemplazarla. Su contenido esta limitado por el tipo EditorTool, que es un tipo de union de strings. Esto significa que solo puede contener uno de los valores especificados en el tipo EditorTool. El valor por defecto es SELECT.
+  readonly selectedTool = signal<EditorTool>('SELECT');
+
+  //Esto es una señal readonly que evita reemplazarla. Su contenido esta limitado por el tipo RacingLinePoint[], que es un array de objetos RacingLinePoint. Esto significa que solo puede contener un array de objetos que cumplan con la interfaz RacingLinePoint. El valor por defecto es un array vacio.
+  readonly points = signal<RacingLinePoint[]>([]);
+
+  //Esto es una señal readonly que evita reemplazarla. Su contenido esta limitado por el tipo string | null, que significa que puede contener un string o null. El valor por defecto es null.
+  readonly selectedPointId = signal<string | null>(null);
+
+  toolUpdate(tool: EditorTool): void {
+    this.selectedTool.set(tool);
+  }
+}
