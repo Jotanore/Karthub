6d02dbc6245d7db2dadd85d4d84a9edac2da6f4c
Jotanore <jotanore@hotmail.com>
2026-08-05 18:03:18 +0200

    Pointer Down event

--- frontend/src/app/components/svg-editor/svg-editor.ts
+++ frontend/src/app/components/svg-editor/svg-editor.ts
@@ -1,5 +1,8 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { CircuitLayout } from '../../models/circuit-layout.model';
+import { RacingLinePoint } from '../../models/racing-line-point.model';
+import { EditorTool } from '../../models/editor-tool.model';
+import { PointCoordinates } from '../../models/point-coordinates.model';
 
 @Component({
   selector: 'app-svg-editor',
@@ -10,5 +13,31 @@ import { CircuitLayout } from '../../models/circuit-layout.model';
 export class SvgEditor {
 
   readonly layout = input.required<CircuitLayout>();
+  readonly points = input.required<readonly RacingLinePoint[]>();
+  readonly activeTool = input.required<EditorTool>();
+  readonly selectedPointId = input.required<string | null>();
 
+  readonly pointCreated = output<PointCoordinates>();
+
+  onSvgPointerDown(event: PointerEvent): void {
+    if (this.activeTool() !== 'ADD_POINT') {
+      return;
+    }
+
+    const svgElement = event.currentTarget as SVGSVGElement;
+    const screenMatrix = svgElement.getScreenCTM();
+    if (!screenMatrix) {
+      console.error('Failed to get screen CTM for SVG element.');
+      return;
+    }
+    const svgPoint = svgElement.createSVGPoint();
+    svgPoint.x = event.clientX;
+    svgPoint.y = event.clientY;
+
+    const transformedPoint = svgPoint.matrixTransform(screenMatrix.inverse());
+
+    this.pointCreated.emit({ x: transformedPoint.x, y: transformedPoint.y });
+
+
+  }
 }
